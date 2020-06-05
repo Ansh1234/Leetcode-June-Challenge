9780f890579fe3f4985908b41ff5c135b193f302
Anshul Jain <anshul.jain010@gmail.com>
2020-06-05 16:32:08 +0530

    Create June_1.py
    Invert a binary tree.

---
+++ June_1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        self.traverse(root)
+        return root
+        
+        
+    def traverse(self, node):
+        if (node == None):
+            return
+        self.traverse(node.left)
+        self.traverse(node.right)
+        node.left, node.right = node.right, node.left
