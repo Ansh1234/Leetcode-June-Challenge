c48fd15e69bedcd58753d1dbda97d72ef77532cd
Anshul Jain <anshul.jain010@gmail.com>
2020-06-05 16:42:41 +0530

    Create June_3.py
    Two City Scheduling

---
+++ June_3.py
@@ -0,0 +1,13 @@
+class Solution:
+  
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        
+        cost_diff = [x[0]- x[1] for x in costs]
+        
+        sorted_costs = [y for x,y in sorted(zip(cost_diff, costs))]
+        
+        length = int(len(costs)/2)
+                
+        total_cost = sum([x[0] for x in sorted_costs[:length]]) + sum([x[1] for x in sorted_costs[length:]])
+            
+        return total_cost
